08dc786d6e415cdc6fe07f17a2c8506104f762fe
Marc Jakobi <marc.jakobi@tiko.energy>
2023-12-25 22:04:21 +0100

    fix: prevent plugin from being sourced more than once

--- plugin/rocks-git.lua
+++ plugin/rocks-git.lua
@@ -1,3 +1,6 @@
+if vim.g.did_load_rocks_git_nvim then
+    return
+end
 local rocks = require("rocks.api")
 local rocks_git = require("rocks-git")
 
@@ -5,3 +8,4 @@ rocks.register_rock_handler({
     get_sync_callback = rocks_git.get_sync_callback,
     get_prune_callback = rocks_git.get_prune_callback,
 })
+vim.g.did_load_rocks_git_nvim = true
