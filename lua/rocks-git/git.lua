ec3da19f449d3a0d18b01d58682213fd88edaf23
Marc Jakobi <marc@jakobi.dev>
2024-01-01 22:59:28 +0100

    fix(sync): checkout remote HEAD if not checked out and no rev is set (#11)

--- lua/rocks-git/git.lua
+++ lua/rocks-git/git.lua
@@ -181,4 +181,16 @@ function git.get_rev(pkg)
     return tag or read_line(vim.fs.joinpath(git_dir, head_ref))
 end
 
+---@param pkg Package
+---@return string | nil head The remote HEAD branch name
+function git.get_head_branch(pkg)
+    local git_dir = vim.fs.joinpath(pkg.dir, ".git")
+    local remote_head_ref = read_line(vim.fs.joinpath(git_dir, "refs", "remotes", "origin", "HEAD"))
+    if not remote_head_ref then
+        return
+    end
+    remote_head_ref = remote_head_ref:gsub("ref: refs/remotes/origin/", "")
+    return remote_head_ref
+end
+
 return git
